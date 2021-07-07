6b6bf994fcb9fb3b0360e4336d21b57c5c3a957a
Erich <35889973+ErichHeinemann@users.noreply.github.com>
2021-07-07 19:44:11 +0200

    Create sequencer.ino

---
+++ ESP32Core0_TEST_I2C_ssd1306_ADS11115_MIDI_PCF8574/sequencer.ino
@@ -0,0 +1,6 @@
+// 2021-07-05 E.Heinemann, this file covers all functions of the sequencer. probably I have to create the sequencer and tracks as classes - perhaps as a structure.
+
+void sequencer_start(){
+    
+  
+}
