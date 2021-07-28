6c448c4339a7d8412f44b1514caacdcd2613073e
Erich <35889973+ErichHeinemann@users.noreply.github.com>
2021-07-28 21:15:03 +0200

    Update menu.ino

--- ESP32Core0_TEST_I2C_ssd1306_ADS11115_MIDI_PCF8574/menu.ino
+++ ESP32Core0_TEST_I2C_ssd1306_ADS11115_MIDI_PCF8574/menu.ino
@@ -8,18 +8,36 @@
 
 
 // 
-void func_but( uint8_t step_number ){
-  Serial.print( step_number );
-  Serial.println( "  func_but");
+void func1_but( uint8_t step_number ){
+  Serial.print( "  act menu: ");
+  Serial.print( act_menuNum);
+  Serial.print( "  func_but:");
+  Serial.println( step_number );
+
   // Instrument-Menu
   if( act_menuNum == 0 ){
-    // select a Instrumen 
+    
+    if( step_number < 12 ){
+      // select a Instrumen 
+      sequencer_new_instr( step_number  );
+    }else{
+      
+      // mute / unmute Instrument
+      if( step_number == 12 ){
+        Serial.print( "  mute:");
+        Serial.println( act_instr );
+        is_muted[act_instr] = ! is_muted[act_instr];  
+        return;
+      } 
+         
+    } 
   }
+
   // Sound-Menu
   if( act_menuNum == 1 ){
-    // select a Instrumen 
+    
   }
-  // Global-Menu
+  // Global-Effects Menu
   if( act_menuNum == 2 ){
     // select a Instrumen 
   }
@@ -52,14 +70,82 @@ void func_but( uint8_t step_number ){
 
 }
 
+
+void func2_but( uint8_t step_number ){
+ // 
+ Serial.print("Func2:"); 
+ Serial.println( step_number ); 
+
+  if( step_number <8 ){
+    changeMenu( step_number );  
+  }
+
+  if( step_number == 15 ){
+    Serial.print( "  next Menu:");
+    Serial.println( act_instr );
+    act_menuNum = act_menuNum+1; 
+    changeMenu(); 
+    return;
+  } 
+  if( step_number == 14 ){
+    Serial.print( "  prevMenu:");
+    Serial.println( act_instr );
+    if( act_menuNum > 0 ){
+      act_menuNum = act_menuNum-1;
+    }else{
+      act_menuNum = act_menuNum_max;
+    } 
+    changeMenu(); 
+    return;
+  } 
+}
+
 // active_track_num
 // active_track_name
 
 void changeMenu(){
-  act_menu = menus[ act_menuNum ];
-  Serial.println( "  changeMenu()");
+  if( act_menuNum > act_menuNum_max ){
+    act_menuNum = 0;
+  } 
+  changeMenu(act_menuNum);
+  // act_menu = menus[ act_menuNum ];
+  //Serial.println( "  changeMenu()");
 }
 
 void changeMenu( uint8_t new_menu_num ){
   Serial.println( "  changeMenu( uint8_t new_menu_num )");
+  val1_synced = false;
+  val2_synced = false;
+  val3_synced = false;
+  val0_synced = false;
+  
+  act_menuNum = new_menu_num;
+  if( act_menuNum > act_menuNum_max ){
+    act_menuNum = act_menuNum_max;
+  } 
+
+  // Instrument-Menu
+  if( act_menuNum ==0 ){    
+    // patch_val0 = bpm_pot_midi;
+    // patch_val1 = bpm_pot_fine_midi;
+  }
+
+  // Speed-Menu
+  if(  act_menuNum ==4 ){    
+    // Main-Speed
+    patch_val0 = bpm_pot_midi;
+    // FIne_Speed
+    patch_val1 = bpm_pot_fine_midi;
+  }
+  
+  param_name0 = pages[ act_menuNum*4    ];
+  param_name1 = pages[ act_menuNum*4 +1 ];
+  param_name2 = pages[ act_menuNum*4 +2 ];
+  param_name3 = pages[ act_menuNum*4 +3 ];
+  
+  act_menu = menus[ act_menuNum ];
+  Serial.println( "  changeMenu()");
+  
 }
+
+
